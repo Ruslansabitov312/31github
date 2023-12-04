3379c5a12589a743551f8083518784f4475fc8f2
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2023-12-04 21:51:45 +0600

    Add routing, lazy chunks & code splitting, bundleAnalyzer plugin, historyApiFallback

--- webpack/src/components/App.tsx
+++ webpack/src/components/App.tsx
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import cls from './App.module.scss'
+import {Link, Outlet} from "react-router-dom";
 
 export const App = () => {
     const [count, setCount] = useState(0);
@@ -7,8 +8,13 @@ export const App = () => {
     const increment = () => setCount(prev => prev + 1);
     return (
         <div>
+            <div className={cls.Navbar}>
+                <Link to={'/about'}>About</Link>
+                <Link to={'/shop'}>Shop</Link>
+            </div>
             <h1 className={cls.Value}>{count}</h1>
             <button className={cls.Button} onClick={increment}>click</button>
+            <Outlet />
         </div>
     );
 };
