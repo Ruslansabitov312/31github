0df88c53af8f3660a6711dce7face38f509d1a83
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2023-12-05 02:52:42 +0600

    Get started with Assets(images, icons, fonts) & add loaders and types

--- webpack/src/components/App/App.tsx
+++ webpack/src/components/App/App.tsx
@@ -1,6 +1,9 @@
 import {useState} from 'react';
 import cls from './App.module.scss'
 import {Link, Outlet} from "react-router-dom";
+import flagKgz from '@/assets/kgz.png'
+import bishkek from '@/assets/Bishkek.jpg'
+import SvgLogo from '@/assets/svglogo.svg'
 
 export const App = () => {
     const [count, setCount] = useState(0);
@@ -8,6 +11,11 @@ export const App = () => {
     const increment = () => setCount(prev => prev + 1);
     return (
         <div>
+            <div className={cls.ImgWrapper}>
+                <img width={350} height={200} src={flagKgz} alt="flag" />
+                <img width={350} height={200} src={bishkek} alt="bishkek" />
+                <SvgLogo width={350} height={200} fill={'red'} style={{color: 'yellow'}} />
+            </div>
             <div className={cls.Navbar}>
                 <Link to={'/about'}>About</Link>
                 <Link to={'/shop'}>Shop</Link>
