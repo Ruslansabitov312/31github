1b34ad5cd39601eb81f3fd0714b73e79cd10b217
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2023-12-17 19:39:23 +0600

    Add dijkstra's algorithm

--- algorithms_and_data_structures/7_breadth_first_search.js
+++ algorithms_and_data_structures/7_breadth_first_search.js
@@ -10,22 +10,22 @@ graph.f = ['g']
 
 function breadthSearch(graph, start, end) {
     let queue = []
-    queue.push(start)
+    queue.push(start) // queue = ['a']
 
-    while (queue.length > 0) {
-        const current = queue.shift()
+    while (queue.length > 0) { // true
+        const current = queue.shift() // current = 'a' (queue = [])
 
-        if(!graph[current]) {
+        if(!graph[current]) { // false - (graph[current] = ['b', 'c'])
             graph[current] = []
         }
 
-        if(graph[current].includes(end)) {
+        if(graph[current].includes(end)) { // false - ( ['b', 'c'].includes('g') )
             return true
         } else {
-            queue = [...queue, ...graph[current]]
+            queue = [...queue, ...graph[current]] // [...[], ...['b', 'c']]
         }
     }
     return false
 }
 
-console.log(breadthSearch(graph, 'a', 'd'))
+console.log(breadthSearch(graph, 'a', 'g'))
