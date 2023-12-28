42d93affbe75ee45e4aa80ebeba8e61abcba42d6
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2023-12-28 18:01:13 +0600

    Add maxProfit function

---
+++ tasks/7_maxProfit.js
@@ -0,0 +1,15 @@
+let prices1 = [7, 1, 5, 3, 6, 4];
+let prices2 = [7, 6, 4, 3, 1];
+
+let maxProfit = function (prices) {
+    let result = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            result += prices[i] - prices[i - 1];
+        }
+    }
+    return result;
+};
+
+console.log(maxProfit(prices1))
