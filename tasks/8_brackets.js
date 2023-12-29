483774885b5ee00e13e93e1d789590cffcfd03f8
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2023-12-29 20:16:26 +0600

    Add brackets function

---
+++ tasks/8_brackets.js
@@ -0,0 +1,23 @@
+function isValid(s) {
+    const brackets = {
+        ")": "(",
+        "]": "[",
+        "}": "{"
+    };
+
+    const st = [];
+    for (let i = 0; i < s.length; i++) {
+        if (isClosedBracket(s[i])) {
+            if (brackets[s[i]] !== st.pop()) return false;
+        } else {
+            st.push(s[i]);
+        }
+    }
+    return st.length === 0;
+}
+
+function isClosedBracket(ch) {
+    return [")", "]", "}"].indexOf(ch) > -1;
+}
+
+console.log(isValid('{)'))
